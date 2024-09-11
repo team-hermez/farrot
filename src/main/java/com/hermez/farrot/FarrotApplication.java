9b7e4f0956e521e8377c392e97625f03f404772b
HS-hyeokjin <youngurwls@gmail.com>
2024-09-11 10:40:59 +0900

    init: 프로젝트 초기화

---
+++ src/main/java/com/hermez/farrot/FarrotApplication.java
@@ -0,0 +1,13 @@
+package com.hermez.farrot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarrotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarrotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hermez/farrot/FarrotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hermez.farrot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarrotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
