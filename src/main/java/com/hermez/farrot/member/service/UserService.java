2e31a0dbdb0639a00c96870be982bf314ac058cb
gitdoolgi <gitdoolgi@gmail.com>
2024-09-11 14:44:13 +0900

    feat: UserService 추가 회원가입 로직 service

---
+++ src/main/java/com/hermez/farrot/member/service/UserService.java
@@ -0,0 +1,25 @@
+package com.hermez.farrot.member.service;
+
+import com.hermez.farrot.member.dto.request.MemberRegisterRequest;
+import com.hermez.farrot.member.entity.Member;
+import com.hermez.farrot.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+    private final MemberRepository memberRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public int save(MemberRegisterRequest memberRegisterRequest) {
+        return memberRepository.save(Member.builder()
+                .memberName(memberRegisterRequest.getMemberName())
+                .email(memberRegisterRequest.getEmail())
+                .password(bCryptPasswordEncoder.encode(memberRegisterRequest.getPassword()))
+                .phone(memberRegisterRequest.getPhone())
+                .nickname(memberRegisterRequest.getNickname())
+                .build()).getMemberId();
+    }
+}
