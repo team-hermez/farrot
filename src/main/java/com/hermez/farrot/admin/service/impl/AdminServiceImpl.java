d8950a05cb9b039ed37c58f8f10b86ab29fe8d51
kde0707 <gammaeom1@gmail.com>
2024-09-18 00:37:18 +0900

    feat: 관리자 상품관리 페이징
    Server
    - AdminController.java
    - AdminRepositoryCustom.java
    - AdminRepositoryCustomImpl.javaI.java
    - AdminService.java
    - AdminServiceImpl.java
    
    View
    - admin-product.html
    - admin-product-today.html
    - admin-product-weekly.html
    - admin-product-monthly.html

--- src/main/java/com/hermez/farrot/admin/service/impl/AdminServiceImpl.java
+++ src/main/java/com/hermez/farrot/admin/service/impl/AdminServiceImpl.java
@@ -4,21 +4,31 @@ import com.hermez.farrot.admin.dto.AdminCategorySalesTop5Response;
 import com.hermez.farrot.admin.repository.AdminRepository;
 import com.hermez.farrot.admin.service.AdminService;
 import com.hermez.farrot.member.entity.Member;
+import com.hermez.farrot.product.entity.Product;
+import com.hermez.farrot.product.repository.ProductRepository;
+import com.hermez.farrot.product.repository.ProductStatusRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
 public class AdminServiceImpl implements AdminService {
 
     public final AdminRepository adminRepository;
+    public final ProductRepository productRepository;
+    public final ProductStatusRepository productStatusRepository;
 
-    public AdminServiceImpl(AdminRepository adminRepository) {
+    public AdminServiceImpl(AdminRepository adminRepository, ProductRepository productRepository, ProductStatusRepository productStatusRepository) {
         this.adminRepository = adminRepository;
+        this.productRepository = productRepository;
+        this.productStatusRepository = productStatusRepository;
     }
 
-    public List<Member> getMemberList(){
-        return adminRepository.findMemberByOrderById();
+    public Page<Member> getMemberList(Pageable pageable){
+        return adminRepository.findMemberByOrderById(pageable);
     }
 
     public int getMemberTotalCount(){
@@ -30,4 +40,12 @@ public class AdminServiceImpl implements AdminService {
         return adminRepository.findCategorySales();
     }
 
+    public Page<Product> getProductList(Pageable pageable) {
+        return productRepository.findAllByOrderByCreatedAtDesc(pageable);
+    }
+
+    public Page<Product> findProductsSoldToday(Pageable pageable) {
+        LocalDate today = LocalDate.now();
+        return adminRepository.findProductsSoldToday(today, pageable);
+    }
 }
