d1aacd82a2e955dc5aa7addab1fa8cde8db6cee0
gitdoolgi <gitdoolgi@gmail.com>
2024-09-17 20:22:47 +0900

    fix: MemberController refactoring 완료, logout 기능 추가

--- src/main/java/com/hermez/farrot/member/controller/MemberController.java
+++ src/main/java/com/hermez/farrot/member/controller/MemberController.java
@@ -1,51 +1,64 @@
 package com.hermez.farrot.member.controller;
 
+import com.hermez.farrot.member.dto.request.MemberLoginRequest;
 import com.hermez.farrot.member.dto.request.MemberRegisterRequest;
 import com.hermez.farrot.member.repository.MemberRepository;
 import com.hermez.farrot.member.service.UserService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RequestMapping("/member")
 @Controller
 public class MemberController {
-
-    private final MemberRepository memberRepository;
-    private final BCryptPasswordEncoder passwordEncoder;
     private final UserService userService;
 
-    public MemberController(MemberRepository memberRepository, BCryptPasswordEncoder passwordEncoder, UserService userService) {
-        this.memberRepository = memberRepository;
-        this.passwordEncoder = passwordEncoder;
+    public MemberController(MemberRepository memberRepository, UserService userService) {
         this.userService = userService;
     }
 
     @GetMapping("/register")
-    String memberRegisterPage(Model model) {
+    public String memberRegisterPage(Model model) {
         return "member/register";
     }
 
     @PostMapping("/register")
-    String postMemberRegister(MemberRegisterRequest memberRegisterRequest) {
+    public String postMemberRegister(MemberRegisterRequest memberRegisterRequest) {
         userService.save(memberRegisterRequest);
-
         return "redirect:/login";
     }
     @GetMapping("/login")
-    String memberLoginPage(Model model) {
+    public String login(@AuthenticationPrincipal UserDetails userDetails) {
+        // 로그인 상태면 메인 페이지로 돌려보냄
+        if(userDetails != null){
+            return "redirect:/";
+        }
         return "member/login";
     }
 
     @PostMapping("/login")
-    String postMember(Model model) {
-        return "member/login";
+    public String logIn(MemberLoginRequest memberLoginRequest, HttpServletResponse response) {
+        userService.logIn(memberLoginRequest, response);
+
+        return "redirect:/";
     }
 
+    @GetMapping("/logout")
+    public String logout(@CookieValue(value = "Authorization", defaultValue="",required = false)
+                             Cookie cookie, HttpServletResponse response){
+        cookie.setValue(null);
+        cookie.setMaxAge(0);
+        cookie.setPath("/");
+        response.addCookie(cookie);
 
+        return "redirect:/member/login";
+    }
     @GetMapping("/detail")
     String memberDetailPage(Model model) {
         return "member/detail";
