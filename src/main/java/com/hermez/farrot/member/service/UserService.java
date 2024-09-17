f9714cc25acadf40f0d3358a907741537c073138
gitdoolgi <gitdoolgi@gmail.com>
2024-09-17 20:34:43 +0900

    feature: UserService
    MemberService의 구현체 JWT를 사용한 기능로직 추가

--- src/main/java/com/hermez/farrot/member/service/UserService.java
+++ src/main/java/com/hermez/farrot/member/service/UserService.java
@@ -1,25 +1,64 @@
 package com.hermez.farrot.member.service;
 
+import com.hermez.farrot.member.dto.request.MemberLoginRequest;
 import com.hermez.farrot.member.dto.request.MemberRegisterRequest;
 import com.hermez.farrot.member.entity.Member;
+import com.hermez.farrot.member.security.JwtTokenProvider;
 import com.hermez.farrot.member.repository.MemberRepository;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
-public class UserService {
+@Transactional
+@Slf4j
+public class UserService implements MemberService{
     private final MemberRepository memberRepository;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final PasswordEncoder passwordEncoder;
 
+    @Transactional
+    @Override
     public int save(MemberRegisterRequest memberRegisterRequest) {
         return memberRepository.save(Member.builder()
                 .memberName(memberRegisterRequest.getMemberName())
                 .email(memberRegisterRequest.getEmail())
-                .password(bCryptPasswordEncoder.encode(memberRegisterRequest.getPassword()))
+                .password(passwordEncoder.encode(memberRegisterRequest.getPassword()))
                 .phone(memberRegisterRequest.getPhone())
                 .nickname(memberRegisterRequest.getNickname())
                 .build()).getId();
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public void logIn(MemberLoginRequest memberLoginRequest, HttpServletResponse response) {
+
+        Optional<Member> optionalMember = memberRepository.findByEmail(memberLoginRequest.getEmail());
+
+        if (optionalMember.isEmpty()){
+            log.warn("회원이 존재하지 않음");
+        }
+
+        Member member = optionalMember.get();
+
+        if(!passwordEncoder.matches(memberLoginRequest.getPassword(), member.getPassword())){
+            log.warn("비밀번호가 일치하지 않습니다.");
+        }
+
+        Cookie cookie = new Cookie(JwtTokenProvider.AUTHORIZATION_HEADER, jwtTokenProvider.generateToken(member.getEmail()));
+        cookie.setMaxAge(7*24*60*60);
+        cookie.setPath("/");
+        cookie.setDomain("localhost");
+        cookie.setSecure(false);
+
+        System.out.println(cookie);
+        response.addCookie(cookie);
     }
 }
