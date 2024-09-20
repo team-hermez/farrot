5718f48113c515aff19e65cf6799eaed58fa235a
kde0707 <gammaeom1@gmail.com>
2024-09-20 23:57:22 +0900

    feat: 관리자 실시간 등록 차트 링크 연결
    View
     - admin-form.html
     - findTop5Latest.js

--- src/main/resources/static/js/admin/findTop5Latest.js
+++ src/main/resources/static/js/admin/findTop5Latest.js
@@ -1,35 +1,34 @@
-
-    function fetchProducts() {
+function fetchProducts() {
     fetch('/admin/latest-products')
         .then(response => response.json())
         .then(data => {
             const toolboxList = document.querySelector('.title1');
-            toolboxList.innerHTML = '';
+            toolboxList.innerHTML = ''; // 기존 내용 초기화
 
             data.forEach(product => {
                 const listItem = `
-
                     <ul class="list-unstyled timeline widget">
-                        <div class="block">
-                            <div class="block_content">
-                                <h2 class="title">
-                                    <a href="#">${product.productName}</a>
-                                </h2>
-                                <div class="byline">
-                                    <span>${new Date(product.createdAt).toLocaleString()}</span> by <a>${product.member.memberName}</a>
+                        <li>
+                            <div class="block">
+                                <div class="block_content">
+                                    <h2 class="title">
+                                        <a href="/product/product-detail?productId=${product.id}">
+                                            ${product.productName}
+                                        </a>
+                                    </h2>
+                                    <div class="byline">
+                                        <span>${new Date(product.createdAt).toLocaleString()}</span> by <a>${product.member.memberName}</a>
+                                    </div>
+                                    <p class="excerpt">${product.description}</p>
                                 </div>
-                                <p class="excerpt">${product.description}</p>
                             </div>
-                        </div>
-
-</ul>
-</div>
-</div>`;
-
+                        </li>
+                    </ul>`;
 
                 toolboxList.innerHTML += listItem;
             });
-        });
+        })
+        .catch(error => console.error('Error fetching products:', error));
 }
 
-    setInterval(fetchProducts, 5000);
+setInterval(fetchProducts, 5000);
