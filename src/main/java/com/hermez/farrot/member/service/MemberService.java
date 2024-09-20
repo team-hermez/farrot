11415e1afb728a803f337653a11f0b4b7887a2bd
gitdoolgi <gitdoolgi@gmail.com>
2024-09-20 10:03:26 +0900

    feature : MemberService 회원정보 불러오기 로직 추가

--- src/main/java/com/hermez/farrot/member/service/MemberService.java
+++ src/main/java/com/hermez/farrot/member/service/MemberService.java
@@ -2,9 +2,11 @@ package com.hermez.farrot.member.service;
 
 import com.hermez.farrot.member.dto.request.MemberLoginRequest;
 import com.hermez.farrot.member.dto.request.MemberRegisterRequest;
+import com.hermez.farrot.member.entity.Member;
 import jakarta.servlet.http.HttpServletResponse;
 
 public interface MemberService {
     int save(MemberRegisterRequest memberRegisterRequest);
     void logIn(MemberLoginRequest memberLoginRequest, HttpServletResponse response);
+    Member userDetail(String jwtToken);
 }
