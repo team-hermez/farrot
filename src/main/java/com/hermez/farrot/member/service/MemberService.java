d522312f795cf9a9fbf767d1313c381346b5e4c4
gitdoolgi <gitdoolgi@gmail.com>
2024-09-21 22:41:29 +0900

    feature : MemberService Member image 관련 로직 추가

--- src/main/java/com/hermez/farrot/member/service/MemberService.java
+++ src/main/java/com/hermez/farrot/member/service/MemberService.java
@@ -3,14 +3,17 @@ package com.hermez.farrot.member.service;
 import com.hermez.farrot.member.dto.request.MemberLoginRequest;
 import com.hermez.farrot.member.dto.request.MemberRegisterRequest;
 import com.hermez.farrot.member.dto.request.MemberUpdateRequest;
+import com.hermez.farrot.member.dto.response.MemberImageResponse;
 import com.hermez.farrot.member.entity.Member;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface MemberService {
     int save(MemberRegisterRequest memberRegisterRequest);
     void logIn(MemberLoginRequest memberLoginRequest, HttpServletResponse response, HttpServletRequest request);
     Member userDetail(String jwtToken);
     Member getMember(HttpServletRequest request);
-    boolean updateUserDetail(MemberUpdateRequest memberUpdateRequest);
+    boolean updateUserDetail(MemberUpdateRequest memberUpdateRequest, MultipartFile file);
+    MemberImageResponse userImage(Member member);
 }
