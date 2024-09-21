18a4c086282ad339f60cda6db9328729d05b7c5c
gitdoolgi <gitdoolgi@gmail.com>
2024-09-21 13:39:32 +0900

    fix : MemberService 회원 정보 수정 로직 추가

--- src/main/java/com/hermez/farrot/member/service/MemberService.java
+++ src/main/java/com/hermez/farrot/member/service/MemberService.java
@@ -2,13 +2,15 @@ package com.hermez.farrot.member.service;
 
 import com.hermez.farrot.member.dto.request.MemberLoginRequest;
 import com.hermez.farrot.member.dto.request.MemberRegisterRequest;
+import com.hermez.farrot.member.dto.request.MemberUpdateRequest;
 import com.hermez.farrot.member.entity.Member;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public interface MemberService {
     int save(MemberRegisterRequest memberRegisterRequest);
-    void logIn(MemberLoginRequest memberLoginRequest, HttpServletResponse response);
+    void logIn(MemberLoginRequest memberLoginRequest, HttpServletResponse response, HttpServletRequest request);
     Member userDetail(String jwtToken);
     Member getMember(HttpServletRequest request);
+    boolean updateUserDetail(MemberUpdateRequest memberUpdateRequest);
 }
